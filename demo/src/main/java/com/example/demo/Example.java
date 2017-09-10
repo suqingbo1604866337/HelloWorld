30d4e2d3b3e3a470a76147550f78eea547712798
Sqb <Sqb@192.168.0.101>
2017-09-10 10:28:23 +0800

    第一版

---
+++ demo/src/main/java/com/example/demo/Example.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class Example {
+	
+	public String home() {
+		return "hello world";
+	}
+	
+	
+
+}
